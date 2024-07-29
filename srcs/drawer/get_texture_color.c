e504723e87fe7348cc63d57c2f2ded9297f6da05
shatilovdr <shatilovdr@gmail.com>
2024-07-29 18:21:22 +0300

    add scene files

--- srcs/drawer/get_texture_color.c
+++ srcs/drawer/get_texture_color.c
@@ -6,28 +6,25 @@
 /*   By: dshatilo <dshatilo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 14:30:01 by dshatilo          #+#    #+#             */
-/*   Updated: 2024/07/22 14:32:16 by dshatilo         ###   ########.fr       */
+/*   Updated: 2024/07/29 18:20:22 by dshatilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_color	get_texture_color(t_coord coord, const mlx_image_t *img)
+t_color	get_texture_color(t_coord coord, const mlx_image_t *texture)
 {
-	int		x;
-	int		y;
-	uint8_t	*pixel;
-	t_color	color;
+	t_color		res;
+	int			x;
+	int			y;
+	uint8_t		*pixelstart;
 
-	x = ((int)((1 - coord.loc_x) * img->width) + img->width / 2) % img->width;
-	y = (int)((1 - coord.loc_y) * img->height) % img->height;
-	if (0 <= x && x < (int) img->width && 0 <= y && y < (int) img->height)
-	{
-		pixel = img->pixels + (y * img->width + x) * sizeof(int32_t);
-		color.r = *pixel;
-		color.g = *(pixel + 1);
-		color.b = *(pixel + 2);
-		return (color);
-	}
-	return ((t_color){0, 0, 0});
+	x = (int)(coord.loc_y * texture->width) % texture->width;
+	y = (int)((-1.0 * coord.loc_x + 1) * texture->height) % texture->height;
+	pixelstart = texture->pixels
+		+ (y * texture->width + x) * 4;
+	res.r = (*(pixelstart));
+	res.g = (*(pixelstart + 1));
+	res.b = (*(pixelstart + 2));
+	return (res);
 }
